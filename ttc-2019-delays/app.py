96c84d5e396845a46dc83dd72db2aa2ad834410b
Jay Liu <57325576+superjay5@users.noreply.github.com>
2020-03-19 13:42:53 -0400

    Update app.py

--- ttc-2019-delays/app.py
+++ ttc-2019-delays/app.py
@@ -17,7 +17,7 @@ engine = create_engine('postgres://root:root1234@my-postgres-db.cxwwjsronhkv.ca-
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 ttc_subway_delay = Base.classes.ttc_subway_detail
-station_in_line = Base.classes.stations_in_line_2018_2019
+station_in_line = Base.classes.stations_in_line
 
 # create route that renders index.html template
 @app.route("/")
